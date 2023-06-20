42d5a0ce87316665ba90dbc10d63c97469b6f335
Francisco Nélio <nelio.romero.fn@gmail.com>
2023-06-19 21:49:56 -0300

    node-cidades

--- node/main.js
+++ node/main.js
@@ -1,8 +1,29 @@
-const http = require('http')
-const server = http.createServer((request, reponse) => {
+const http = require('http');
+const fs = require("fs");
+
+function pegaArquivo(nome) {
+
+    fs.readFile('cidades.json', 'utf8' (err,data) => {
+        if (err)
+    })
+
+
+
+
+}
+const server = http.createServer((request, response) => {
     console.log(request.url)
-    response.writeHead()
-    response.end('<h1>olá</h1>')
+    if(request.url == "/hello") {
+    response.end("<h1>Hello World</h1>")
+} else if (request.url == "/cidades") {
+    const arquivo = pegarArquivo()
+    const data = JSON.parse(arquivo)
+    response.setHeader('Content-Type', 'aplication/json')
+    response.end(JSON.stringify(cidades))
+} else {
+        response.statusCode = 404;
+        response.end('<h1>Not found</h1>')
+    }
 });
 
 server.listen(8080, () => {console.log('ouvindo req na porta 8080')})
